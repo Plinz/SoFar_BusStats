ba6dae8d53ad133946ebfc947765659a7cdd9566
Plinz <antoine.duquennoy59@gmail.com>
2015-11-10 10:27:40 +0200

    some small change

--- src/rendu/Ecran.java
+++ src/rendu/Ecran.java
@@ -20,7 +20,6 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
 import org.jdatepicker.impl.JDatePanelImpl;
@@ -32,6 +31,10 @@ import connection.TimePeriod;
 
 public class Ecran extends JFrame{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Container container;
 	private JButton go;
 	private JPanel nord;
@@ -44,8 +47,8 @@ public class Ecran extends JFrame{
 	private JDatePickerImpl datePicker;
 	private JDatePanelImpl datePanel;
 	private final String[] heures= {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"};
-	private JComboBox heuref;
-	private JComboBox heuret;
+	private JComboBox<String> heuref;
+	private JComboBox<String> heuret;
 	private TimePeriod td;
 	private List<LegCollection> list;
 	
@@ -59,105 +62,103 @@ public class Ecran extends JFrame{
 			this.setResizable(false);
 		
 		//instanciation
-			go = new JButton("Search");
-			nord = new JPanel();
-			centre = new JPanel();
+			this.go = new JButton("Search");
+			this.nord = new JPanel();
+			this.centre = new JPanel();
 			//ftime = new JTextField("From time");
 			//ttime = new JTextField("To time");
-			heuref = new JComboBox<String>(heures);
-			heuret = new JComboBox<String>(heures);
-			LocalDateTime a = LocalDateTime.of(2016, 1, 1, 8, 5);
-			LocalDateTime b = LocalDateTime.of(2016, 1, 1, 22, 0);
-				td = new TimePeriod(a,b);
-			
-			
+			this.heuref = new JComboBox<String>(heures);
+			this.heuret = new JComboBox<String>(heures);
 		//JdatePicker
-			model = new UtilDateModel();
-			model.setDate(2015, 10, 5);
-			model.setSelected(true);
+			this.model = new UtilDateModel();
+			this.model.setDate(2015, 10, 5);
+			this.model.setSelected(true);
 		//poperties for JDatePanelImpl
-			p = new Properties();
-			p.put("text.today", "Today");
-			p.put("text.month", "Month");
-			p.put("text.year", "Year");
-			datePanel = new JDatePanelImpl(model, p);
-			datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+			this.p = new Properties();
+			this.p.put("text.today", "Today");
+			this.p.put("text.month", "Month");
+			this.p.put("text.year", "Year");
+			this.datePanel = new JDatePanelImpl(model, p);
+			this.datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
 			
 		//Panel nord
-			nord.setSize(900,100);
-			nord.setLayout(new GridLayout(1, 4));
-			nord.add(datePicker);
-			nord.add(heuref);
-			nord.add(heuret);
-			nord.add(go);
-			nord.setVisible(true);
+			this.nord.setSize(900,100);
+			this.nord.setLayout(new GridLayout(1, 4));
+			this.nord.add(datePicker);
+			this.nord.add(heuref);
+			this.nord.add(heuret);
+			this.nord.add(go);
+			this.nord.setVisible(true);
 		
 		//Combo box
-			heuref.setSelectedItem(6);
-			heuret.setSelectedItem(22);
-			heuref.addActionListener (new ActionListener () {
-			    public void actionPerformed(ActionEvent e) {
-			    	 heuref = (JComboBox)e.getSource();
-			         String tmp = (String)heuref.getSelectedItem();
-			         hfrom = Integer.parseInt(tmp);
+			this.heuref.setSelectedItem(6);
+			this.heuret.setSelectedItem(22);
+			this.heuref.addActionListener (new ActionListener () {
+			    @SuppressWarnings("unchecked")
+				public void actionPerformed(ActionEvent e) {
+			    	heuref = ((JComboBox<String>)e.getSource());
+			        String tmp = (String)heuref.getSelectedItem();
+			        hfrom = Integer.parseInt(tmp);
 			    }
 			});
-			heuret.addActionListener (new ActionListener () {
-			    public void actionPerformed(ActionEvent e) {
-			    	 heuret = (JComboBox)e.getSource();
+			this.heuret.addActionListener (new ActionListener () {
+			    @SuppressWarnings("unchecked")
+				public void actionPerformed(ActionEvent e) {
+			    	 heuret = (JComboBox<String>)e.getSource();
 			         String tmp2 = (String)heuret.getSelectedItem();
 			         hto = Integer.parseInt(tmp2);
 			    }
 			});
 				
 		//table & centre 
-			centre.setLayout(new BorderLayout());
+			this.centre.setLayout(new BorderLayout());
 			String[][] donnees ={};	
 		    //                  String    String   hashMap      int           TRavelLEgs  Long
 		    String[] entetes = {"PointA","PointB","Stop 1-5","Stop 5-10","Stop > 10 ","TravelTime","Distance"};
 		    TableDonnee tablemodel = new TableDonnee(donnees,entetes);
-		    tableau = new JTable(tablemodel);
-		    centre.add(tableau.getTableHeader(), BorderLayout.NORTH);
-		    centre.add(tableau, BorderLayout.CENTER);
-		    centre.setSize(500,500);
-			centre.setBackground(Color.BLACK);
-			centre.setVisible(true);
+		    this.tableau = new JTable(tablemodel);
+		    this.centre.add(tableau.getTableHeader(), BorderLayout.NORTH);
+		    this.centre.add(tableau, BorderLayout.CENTER);
+		    this.centre.setSize(500,500);
+			this.centre.setBackground(Color.BLACK);
+			this.centre.setVisible(true);
 
 			// search
-			go.addActionListener(new ActionListener()
+			this.go.addActionListener(new ActionListener()
 			{
 			    public void actionPerformed(ActionEvent e)
 			    {
+
 			    	LocalDate selectedDate = ((Date)datePicker.getModel().getValue()).toInstant().atZone(ZoneId.of("Europe/Athens")).toLocalDate();
-			       System.out.println("Date : "+selectedDate+"\n"+
-			        					"from : "+hfrom+"\n"+
-			        					"to : "+hto+"\n");
-			       LocalDateTime fromtmp = selectedDate.atStartOfDay().plusHours(hfrom);
-			       LocalDateTime totmp = selectedDate.atStartOfDay().plusHours(hto);
-			       td = new TimePeriod(fromtmp,totmp);
-			       list = td.getTravelStats();
-			       String[][] donnees = new String[0][7];
-			       String[] rowData = new String[7];
-			       tablemodel.setDataVector(donnees, entetes);
-				   for(int i = 0; i < list.size() ; i++ ){
-				    	rowData[0] = list.get(i).getPointA().getName();
-				    	rowData[1] = list.get(i).getPointB().getName();
-				    	rowData[2] = list.get(i).getAvgStopsByTime().get(Duration.ofMinutes(1))+"";
-				    	rowData[3] = list.get(i).getAvgStopsByTime().get(Duration.ofMinutes(5))+"";
-				    	rowData[4] = list.get(i).getAvgStopsByTime().get(Duration.ofMinutes(10))+"";
-				    	Duration tmp =list.get(i).getAvgTravelTime();
-				    	rowData[5] = ""+(int)tmp.toHours()+"h "+(int)tmp.toMinutes()%60+"m "+(int)tmp.toMinutes()%60+"s";
-				    	rowData[6] = ((int)list.get(i).getAvgDistance())+" meters";
-				    	tablemodel.addRow(rowData);
-				    }
+				    System.out.println("Date : "+selectedDate+"\n"+
+				    					"from : "+hfrom+"\n"+
+				    					"to : "+hto+"\n");
+				    LocalDateTime fromtmp = selectedDate.atStartOfDay().plusHours(hfrom);
+				    LocalDateTime totmp = selectedDate.atStartOfDay().plusHours(hto);
+				    td = new TimePeriod(fromtmp,totmp);
+				    list = td.getTravelStats();
+				    String[][] donnees = new String[0][7];
+				    String[] rowData = new String[7];
+				    tablemodel.setDataVector(donnees, entetes);
+					for(int i = 0; i < list.size() ; i++ ){
+					 	rowData[0] = list.get(i).getPointA().getName();
+					   	rowData[1] = list.get(i).getPointB().getName();
+					   	rowData[2] = list.get(i).getAvgStopsByTime().get(Duration.ofMinutes(1))+"";
+					   	rowData[3] = list.get(i).getAvgStopsByTime().get(Duration.ofMinutes(5))+"";
+					   	rowData[4] = list.get(i).getAvgStopsByTime().get(Duration.ofMinutes(10))+"";
+					   	Duration tmp =list.get(i).getAvgTravelTime();
+					   	rowData[5] = ""+(int)tmp.toHours()+"h "+(int)tmp.toMinutes()%60+"m "+(int)tmp.toMinutes()%60+"s";
+					   	rowData[6] = ((int)list.get(i).getAvgDistance())+" meters";
+					   	tablemodel.addRow(rowData);
+					}
 			    }
 			});;
 		
 		//corps
 			this.setLayout(new BorderLayout());
-			container = this.getContentPane();
-			container.add(nord,BorderLayout.NORTH);
-			container.add(centre,BorderLayout.CENTER);
+			this.container = this.getContentPane();
+			this.container.add(nord,BorderLayout.NORTH);
+			this.container.add(centre,BorderLayout.CENTER);
 		
 		
 		
@@ -171,6 +172,10 @@ public class Ecran extends JFrame{
 	}
 	
 	class TableDonnee extends DefaultTableModel{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		private String[][] donnee;
 		private String[] title;
 		
@@ -187,7 +192,7 @@ public class Ecran extends JFrame{
 	    	return this.title.length;
 	    }          
 	}
-	
+
 	/*//tracer une ligne
 	 * public void paintcomponent(Graphics g) {
         super.paintComponents(g);
